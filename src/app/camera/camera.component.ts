68b072398fe5a2ab91b312de8decbcad463f55d6
AlexGat27 <aleksandrgataulin745@gmail.com>
2024-02-16 10:59:33 +0300

    Работа с компонентом камеры

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,18 +1,21 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CameraService } from '../services/camera.service';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
-  styleUrl: './camera.component.css'
+  styleUrl: './camera.component.css',
 })
 export class CameraComponent implements AfterViewInit{
   @ViewChild("videoPlayback") videoElementRef: ElementRef;
   isCameraActive = false;
-  constructor(private cameraService: CameraService){}
+  isGameStart = false;
+  // private hammer: HammerManager;
+  constructor(private cameraService: CameraService,
+              private gameService: GameService){}
 
   ngAfterViewInit(): void {
-    console.log(this.videoElementRef)
     this.OnOffCamera();
   }
 
@@ -26,8 +29,14 @@ export class CameraComponent implements AfterViewInit{
         videoElement.srcObject = stream;
         this.isCameraActive=true;
       }).catch(er => {
-        console.log(`Проблема с камерой, ${er}`);
+        console.log(`Хьюстон у нас проблемы, ${er}`);
       })
     }
   }
+
+  // private handleSwipe(event: HammerInput): void {
+  //   // Обработка жеста swipe на видео
+  //   console.log('Swipe detected on video!', event);
+  //   // Добавьте здесь вашу логику обработки жеста на видео
+  // }
 }
